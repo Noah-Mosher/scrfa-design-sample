52d9018864e6ae71d3a35c4552ef08d7e1622211
Noah Mosher <NoahMosher@Noah-Mosher-MacBook-Air.local>
2022-02-04 19:29:53 -0600

    Remove Preview Prop from AntD Image for Cards

--- src/components/InfoCards.js
+++ src/components/InfoCards.js
@@ -11,25 +11,25 @@ const InfoCards = () => {
   return <div className={styles.CardsContainer}>
     <Card>
       <div className={styles.ImageHolder}>
-        <Image src={data}/>
+        <Image src={data} preview={false}/>
       </div>
       <Typography.Text>DATA & RESEARCH</Typography.Text>
     </Card>
     <Card>
       <div className={styles.ImageHolder}>
-        <Image src={geography}/>
+        <Image src={geography} preview={false}/>
       </div>
       <Typography.Text>GEOPGRAPHY & MAPPING</Typography.Text>
     </Card>
     <Card>
       <div className={styles.ImageHolder}>
-        <Image src={programs}/>
+        <Image src={programs} preview={false}/>
       </div>
       <Typography.Text className={styles.CardLabel}>PROGRAMS & SERVICES</Typography.Text>
     </Card>
     <Card>
       <div className={styles.ImageHolder}>
-        <Image src={legislative}/>
+        <Image src={legislative} preview={false}/>
       </div>
       <Typography.Text>LEGISLATIVE FISCAL IMPACTS</Typography.Text>
     </Card>
